cef2f7624c3d08c24ee4509aaf7ac8e5fc96b95e
esotericpig <esotericpig@users.noreply.github.com>
2019-04-24 03:22:46 +0800

    Initial commit

---
+++ src/test/java/com/esotericpig/jeso/SysTest.java
@@ -0,0 +1,55 @@
+/**
+ * This file is part of jeso.
+ * Copyright (c) 2019 Jonathan Bradley Whited (@esotericpig)
+ * 
+ * jeso is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * jeso is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with jeso. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.esotericpig.jeso;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * <pre>
+ * If a Security Manager exists and disallows certain operations, then some of
+ *   these tests could fail.
+ * </pre>
+ * 
+ * @author Jonathan Bradley Whited (@esotericpig)
+ */
+public class SysTest {
+  @BeforeEach
+  public void setUpEach() {
+  }
+  
+  @AfterEach
+  public void tearDownEach() {
+  }
+  
+  @Test
+  public void testStatics() {
+    System.out.println("OS Name:   " + Sys.OS_NAME);
+    System.out.println("OS Family: " + Sys.OS_FAMILY);
+    
+    assertEquals(Sys.OSFamily.LINUX,Sys.OSFamily.guessFromName("GNU/Linux Fedora"));
+    assertEquals(Sys.OSFamily.MACOS,Sys.OSFamily.guessFromName("Apple Darwin"));
+    assertEquals(Sys.OSFamily.MACOS,Sys.OSFamily.guessFromName("Apple Mac OS X"));
+    assertEquals(Sys.OSFamily.MACOS,Sys.OSFamily.guessFromName("Apple OS X"));
+    assertEquals(Sys.OSFamily.WINDOWS,Sys.OSFamily.guessFromName("Microsoft Windows XP"));
+  }
+}
