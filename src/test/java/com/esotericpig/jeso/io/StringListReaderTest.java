7f5b41fdfaea4b1d5d025a7ed067331d5e7b2faa
esotericpig <esotericpig@users.noreply.github.com>
2019-05-27 20:31:30 +0800

    Initial commit

---
+++ src/test/java/com/esotericpig/jeso/io/StringListReaderTest.java
@@ -0,0 +1,161 @@
+/**
+ * This file is part of jeso.
+ * Copyright (c) 2019 Jonathan Bradley Whited (@esotericpig)
+ * 
+ * jeso is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * jeso is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with jeso. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.esotericpig.jeso.io;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.IOException;
+import java.io.StringReader;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * <pre>
+ * </pre>
+ * 
+ * @author Jonathan Bradley Whited (@esotericpig)
+ */
+public class StringListReaderTest {
+  protected String expectedOutput = null;
+  protected List<String> list = null;
+  protected Random rand = null;
+  
+  @BeforeEach
+  public void setUpEach() {
+    list = new LinkedList<>();
+    rand = new Random();
+    
+    list.add("");
+    list.add("Hello World");
+    list.add(" Hello World ");
+    list.add("");
+    list.add("     Hello World     ");
+    list.add("");
+    
+    StringBuilder sb = new StringBuilder();
+    
+    for(String s: list) {
+      sb.append(s).append('\n');
+    }
+    
+    expectedOutput = sb.toString();
+  }
+  
+  @AfterEach
+  public void tearDownEach() {
+    expectedOutput = null;
+    rand = null;
+    
+    if(list != null) {
+      list.clear();
+      list = null;
+    }
+  }
+  
+  @Test
+  public void testMarkResetSkip() throws IOException {
+    try(StringListReader in = new StringListReader(list)) {
+      StringBuilder output = new StringBuilder();
+      
+      in.mark();
+      for(int i = 0; i < 13; ++i) {
+        output.append((char)in.read());
+      }
+      
+      in.reset();
+      in.skip(13);
+      
+      in.mark();
+      for(int i = 0; i < 5; ++i) {
+        output.append((char)in.read());
+      }
+      
+      in.reset();
+      in.skip(5);
+      
+      int readChar = -1;
+      
+      while((readChar = in.read()) != -1) {
+        output.append((char)readChar);
+      }
+      
+      assertEquals(expectedOutput,output.toString());
+    }
+  }
+  
+  @Test
+  public void testRead() throws IOException {
+    try(StringListReader in = new StringListReader(list)) {
+      StringBuilder output = new StringBuilder();
+      int readChar = -1;
+      
+      while((readChar = in.read()) != -1) {
+        output.append((char)readChar);
+      }
+      
+      assertEquals(expectedOutput,output.toString());
+    }
+  }
+  
+  @Test
+  public void testReadBuffer() throws IOException {
+    System.out.println("[testReadBuffer()]");
+    
+    try(StringListReader in = new StringListReader(list);
+        StringReader sin = new StringReader(expectedOutput)) {
+      final int BUFFER_LEN = 13;
+      
+      StringBuilder output = new StringBuilder();
+      
+      while(true) {
+        char[] buffer = new char[BUFFER_LEN];
+        char[] bufferS = new char[BUFFER_LEN];
+        int offset = rand.nextInt(BUFFER_LEN);
+        int length = rand.nextInt(BUFFER_LEN - offset);
+        
+        int readCount = in.read(buffer,offset,length);
+        sin.read(bufferS,offset,length);
+        
+        if(readCount == -1) {
+          break;
+        }
+        
+        System.out.print("" + readCount + ": '");
+        for(int i = offset; i < (offset + readCount); ++i) {
+          char readChar = buffer[i];
+          
+          System.out.print((readChar == '\n') ? "\\n" : readChar);
+          output.append(readChar);
+        }
+        System.out.println("'");
+        
+        assertArrayEquals(bufferS,buffer);
+      }
+      
+      assertEquals(expectedOutput,output.toString());
+    }
+  }
+}
